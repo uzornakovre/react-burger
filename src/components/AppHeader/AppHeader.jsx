e84ec5553d82df1210adab15bb02e070a0118810
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-21 01:22:23 +0300

    feat: add header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import appHeaderStyles from './AppHeader.module.scss';
+import { 
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
 class AppHeader extends React.Component {
   render() {
     return (
-      <></>
+      <header className={appHeaderStyles.header}>
+        <div className={appHeaderStyles.container}>
+          <nav className={appHeaderStyles.navigation}>
+            <ul className={appHeaderStyles.navList}>
+              <li className={`${appHeaderStyles.navItem} p-5 mr-2`}>
+                <a className={appHeaderStyles.navLink} href='#'>
+                  <BurgerIcon type="primary" />
+                  <span className={`${appHeaderStyles.navLinkText} pl-2`}>Конструктор</span>
+                </a>               
+              </li>
+              <li className={`${appHeaderStyles.navItem} p-5 mr-2`}>
+                <a className={`${appHeaderStyles.navLink} ${appHeaderStyles.inactive}`} href='#'>
+                  <ListIcon type="secondary" />
+                  <span className={`${appHeaderStyles.navLinkText} pl-2`}>Лента заказов</span>
+                </a>               
+              </li>
+              <li className={`${appHeaderStyles.navItem} p-5`}>
+                <a className={`${appHeaderStyles.navLink} ${appHeaderStyles.inactive}`} href='#'>
+                  <ProfileIcon type="secondary" />
+                  <span className={`${appHeaderStyles.navLinkText} pl-2`}>Личный кабинет</span>
+                </a>               
+              </li>
+            </ul>
+          </nav>
+          <div className={appHeaderStyles.logo}><Logo /></div>
+        </div>
+      </header>
     )
   }
 }
