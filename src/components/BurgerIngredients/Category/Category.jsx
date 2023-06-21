098b7b583be09f8851a1e06f7b39cf8f65bde77d
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-22 00:25:46 +0300

    feat: render items

--- src/components/BurgerIngredients/Category/Category.jsx
+++ src/components/BurgerIngredients/Category/Category.jsx
@@ -5,27 +5,8 @@ import Item from '../Item/Item';
 import styles from './Category.module.scss';
 
 class Category extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.items = this.state.items.map((i) => (
-  //     <li className={styles.listItem} key={`listItem-${i._id}`}>
-  //       <Item 
-  //         key={`listItem-${i._id}`}
-  //         name={i.name}
-  //         type={i.type}
-  //         proteins={i.proteins}
-  //         fat={i.fat}
-  //         carbohydrates={i.carbohydrates}
-  //         calories={i.calories}
-  //         price={i.price}
-  //         image={i.image} />
-  //     </li>
-  //   ));
-  // }  
-
   state = {
     items: [],
-    listItems: []
   }
 
   componentDidMount() {
@@ -48,31 +29,11 @@ class Category extends React.Component {
     });
   }
 
-  // componentDidUpdate() {
-  //   this.setState({
-  //     ...this.state,
-  //     listItems: this.state.items.map((i) => (
-  //       <li className={styles.listItem} key={`listItem-${i._id}`}>
-  //         <Item 
-  //           key={`listItem-${i._id}`}
-  //           name={i.name}
-  //           type={i.type}
-  //           proteins={i.proteins}
-  //           fat={i.fat}
-  //           carbohydrates={i.carbohydrates}
-  //           calories={i.calories}
-  //           price={i.price}
-  //           image={i.image} />
-  //       </li>
-  //     ))
-  //   });
-  // }
-
   render() {
     return (
       <div className={`${styles.category}`}>
         <h3 className={styles.title}>{this.props.title}</h3>
-        <ul className={styles.list}>
+        <ul className={`${styles.list} pt-6 pr-4 pb-10 pl-4`}>
           {this.state.items}
         </ul>
       </div>
--- src/components/BurgerIngredients/Item/Item.jsx
+++ src/components/BurgerIngredients/Item/Item.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './Item.module.scss';
 
 class Item extends React.Component {
   render() {
     return (
       <div className={styles.item}>
-        {this.props.name}
+        <img className={`${styles.image} pl-4 pr-4 pb-1`} src={this.props.image} alt={this.props.name} />
+        <div className={styles.price}>
+          <span className={styles.priceValue}>{this.props.price}</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <p className={`${styles.title} pt-1`}>{this.props.name}</p>
       </div>
     )
   }
