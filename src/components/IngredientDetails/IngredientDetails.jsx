38a2b0ef72dd0c0b4a6dcd6202278f64eb032bc0
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-26 22:35:33 +0300

    feat: add details components layout

---
+++ src/components/IngredientDetails/IngredientDetails.jsx
@@ -0,0 +1,9 @@
+import styles from './IngredientDetails.module.scss';
+
+function IngredientDetails() {
+  return (
+    <div className={styles.content}></div>
+  );
+}
+
+export default IngredientDetails;
---
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -0,0 +1,9 @@
+import styles from './OrderDetails.module.scss';
+
+function OrderDetails() {
+  return (
+    <div className={styles.container}></div>
+  );
+}
+
+export default OrderDetails;
