aa1d01c0fddc3599be7c3b6e047fc0ded1ac947b
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-29 21:26:40 +0300

    feat: add order modal

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,8 +1,15 @@
 import styles from './OrderDetails.module.scss';
+import orderCompleteImage from '../../images/done.png';
 
 function OrderDetails() {
   return (
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <h3 className={styles.identifier}>034536</h3>
+      <p className={styles.subtitle}>идентификатор заказа</p>
+      <img className={styles.image} src={orderCompleteImage} alt="Галочка" />
+      <span className={styles.description}>Ваш заказ начали готовить</span>
+      <span className={`${styles.description} ${styles.grey}`}>Дождитесь готовности на орбитальной станции</span>
+    </div>
   );
 }
 
