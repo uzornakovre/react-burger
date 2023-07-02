d445da7d089f1fae8a8fa3538e6b640f325f9ad5
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-07-02 21:48:49 +0300

    feat: install redux

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from 'react';
+// import { compose, createStore, applyMiddleware } from 'redux';
 import { api } from '../../utils/api';
 import styles from './app.module.scss';
 import AppHeader from '../app-header/app-header';
 import Content from '../burger-constructor-page/burger-constructor-page';
+
+// const composeEnhancers =
+//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
+
+// const enhancer = composeEnhancers();
+// const rootReducer = (state, action) => state;
+// const store = createStore(rootReducer, enhancer); 
 
 function App() {
   const [ingredientsList, setIngredientsList] = useState([]);
