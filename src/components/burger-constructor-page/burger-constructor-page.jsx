ed1303e6775ff9537f94acfa229c897dda146686
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-07-11 01:38:36 +0300

    feat: add ingredients counter

--- src/components/burger-constructor-page/burger-constructor-page.jsx
+++ src/components/burger-constructor-page/burger-constructor-page.jsx
@@ -1,14 +1,18 @@
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import styles from './burger-constructor-page.module.scss';
 
 function BurgerConstructorPage() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <BurgerIngredients />
-        <BurgerConstructor />
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div className={styles.container}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </div>
+      </DndProvider>
     </main>
   )
 }
--- src/components/burger-ingredients/item/item.jsx
+++ src/components/burger-ingredients/item/item.jsx
@@ -1,8 +1,29 @@
 import PropTypes from 'prop-types';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './item.module.scss';
+import { useSelector } from 'react-redux';
+import { getSelectedBun, getSelectedIngredients } from '../../../utils/constants';
+import { useEffect, useState } from 'react';
 
-function Item({ name, price, image }) {
+function Item({ name, price, image, _id }) {
+
+  const selectedBun = useSelector(getSelectedBun);
+  const selectedIngredients = useSelector(getSelectedIngredients);
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let ingredCount = 0;
+
+    selectedIngredients.forEach(i => i._id === _id ? ingredCount++ : ingredCount);
+    setCounter(ingredCount);
+
+    if (selectedBun._id === _id) {
+      setCounter(1);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedBun, selectedIngredients])
+
   return (
     <div className={styles.item}>
       <img className={`${styles.image} pl-4 pr-4 pb-1`} src={image} alt={name} />
@@ -11,6 +32,10 @@ function Item({ name, price, image }) {
         <CurrencyIcon type="primary" />
       </div>
       <p className={`${styles.title} pt-1`}>{name}</p>
+      {
+        counter > 0 && 
+        <div className={styles.counter}><span className={styles.counter_value}>{counter}</span></div>
+      }
     </div>
   )
 }
@@ -19,6 +44,7 @@ Item.propTypes = {
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
+  _id: PropTypes.string.isRequired,
 }
 
 export default Item;
