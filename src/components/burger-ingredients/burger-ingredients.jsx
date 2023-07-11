cd48b0427c4420086880ae53108b85d78e2bb5af
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-07-11 23:56:15 +0300

    refactor: replace ingredients menu to custom component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,18 +1,17 @@
-import { useState, useRef } from 'react';
-import Category from './category/category';
+import styles from './burger-ingredients.module.scss';
 import Modal from '../modal/modal';
 import TabMenu from './tab-menu/tab-menu';
 import IngredientDetails from '../ingredient-details/ingredient-details';
-import styles from './burger-ingredients.module.scss';
+import IngredientsMenu from './ingredients-menu/ingredients-menu';
+import { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentIngredient } from '../../services/current-ingredient/currentIngredientSlice';
 import { setIsIngredientDetailsModalOpen } from '../../services/modals/modalsSlice';
 import { getCurrentIngredient, getIsIngredientDetailsModalOpen } from '../../utils/constants';
 
 function BurgerIngredients() {
   const currentIngredient = useSelector(getCurrentIngredient);
   const isIngredientDetailsModalOpen = useSelector(getIsIngredientDetailsModalOpen);
-  const [current, setCurrent] = useState('one');
+  const [currentTab, setCurrentTab] = useState('one');
 
   const dispatch = useDispatch();
 
@@ -20,30 +19,6 @@ function BurgerIngredients() {
   const bunCategoryRef = useRef();
   const saucesCategoryRef = useRef();
   const mainCategoryRef = useRef();
-
-  function handleScrollMenu() {
-    const tabMenuPosition = tabMenuRef.current.getBoundingClientRect().bottom;
-    const bunsPosition = bunCategoryRef.current.getBoundingClientRect().top;
-    const saucesPosition = saucesCategoryRef.current.getBoundingClientRect().top;
-    const mainPosition = mainCategoryRef.current.getBoundingClientRect().top;
-
-    let bunsDistance = Math.abs(tabMenuPosition - bunsPosition);
-    let saucesDistance = Math.abs(tabMenuPosition - saucesPosition);
-    let mainDistance = Math.abs(tabMenuPosition - mainPosition);
-
-    if (bunsDistance <= saucesDistance && bunsDistance <= mainDistance) {
-      setCurrent('one');
-    } else if (saucesDistance <= bunsDistance && saucesDistance <= mainDistance) {
-      setCurrent('two');
-    } else if (mainDistance <= bunsDistance || mainDistance <= saucesDistance) {
-      setCurrent('three');
-    }
-  }
-
-  function handleIngredientClick(item) {
-    dispatch(setCurrentIngredient(item));
-    dispatch(setIsIngredientDetailsModalOpen(true));
-  }
 
   function closeModal() {
     dispatch(setIsIngredientDetailsModalOpen(false));
@@ -53,32 +28,19 @@ function BurgerIngredients() {
     <section className={`${styles.burger_ingredients} mt-10`}>
       <h2 className={styles.title}>Соберите бургер</h2>
       <TabMenu 
-        current={current}
+        currentTab={currentTab}
         tabMenuRef={tabMenuRef}
         bunCategoryRef={bunCategoryRef}
         saucesCategoryRef={saucesCategoryRef}
         mainCategoryRef={mainCategoryRef}
       />
-      <ul className={styles.categories} onScroll={handleScrollMenu}>
-        <li className={styles.categoriesItem} ref={bunCategoryRef}>
-          <Category
-            title="Булка"
-            type="bun"
-            onIngredientClick={handleIngredientClick} />
-        </li>
-        <li className={styles.categoriesItem} ref={saucesCategoryRef}>
-          <Category
-            title="Соусы"
-            type="sauce"
-            onIngredientClick={handleIngredientClick} />
-        </li>
-        <li className={styles.categoriesItem} ref={mainCategoryRef}>
-          <Category
-            title="Начинки"
-            type="main"
-            onIngredientClick={handleIngredientClick} />
-        </li>
-      </ul>
+      <IngredientsMenu
+        setCurrentTab={setCurrentTab}
+        tabMenuRef={tabMenuRef}
+        bunCategoryRef={bunCategoryRef}
+        saucesCategoryRef={saucesCategoryRef}
+        mainCategoryRef={mainCategoryRef}
+      />
       <Modal
         isOpen={isIngredientDetailsModalOpen}
         onClose={closeModal}
--- src/components/burger-ingredients/tab-menu/tab-menu.jsx
+++ src/components/burger-ingredients/tab-menu/tab-menu.jsx
@@ -1,7 +1,13 @@
 import styles from './tab-menu.module.scss';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 
-function TabMenu({ current, tabMenuRef, bunCategoryRef, saucesCategoryRef, mainCategoryRef }) {
+function TabMenu({ 
+  currentTab, 
+  tabMenuRef, 
+  bunCategoryRef, 
+  saucesCategoryRef, 
+  mainCategoryRef 
+}) {
   
   function handleTabClick(ref) {
     ref.current.scrollIntoView({
@@ -12,13 +18,13 @@ function TabMenu({ current, tabMenuRef, bunCategoryRef, saucesCategoryRef, mainC
 
   return (
     <div className={`${styles.tabs} mt-5 mb-10`} ref={tabMenuRef}>
-      <Tab value="one" active={current === 'one'} onClick={() => handleTabClick(bunCategoryRef)}>
+      <Tab value="one" active={currentTab === 'one'} onClick={() => handleTabClick(bunCategoryRef)}>
         Булки
       </Tab>
-      <Tab value="two" active={current === 'two'} onClick={() => handleTabClick(saucesCategoryRef)}>
+      <Tab value="two" active={currentTab === 'two'} onClick={() => handleTabClick(saucesCategoryRef)}>
         Соусы
       </Tab>
-      <Tab value="three" active={current === 'three'} onClick={() => handleTabClick(mainCategoryRef)}>
+      <Tab value="three" active={currentTab === 'three'} onClick={() => handleTabClick(mainCategoryRef)}>
         Начинки
       </Tab>
     </div>
