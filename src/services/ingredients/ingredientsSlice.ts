997da3e79e61c63a8695f9946752d6a4b8334279
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-08-08 01:57:27 +0300

    refactor: migrate ingredients slice to typescript

---
+++ src/services/ingredients/ingredientsSlice.ts
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { fetchIngredients } from '../../utils/api';
+import { UnknownAsyncThunkRejectedAction } from '@reduxjs/toolkit/dist/matchers';
+
+interface IIngredientsState {
+  ingredients: Array<TIngredient>;
+  error: String;
+  isLoading: Boolean;
+}
+
+const initialState: IIngredientsState = {
+  ingredients: [],
+  error: '',
+  isLoading: false,
+}
+
+export const getIngredients = createAsyncThunk('ingredients/getIngredients', async () => {
+  const res = await fetchIngredients().then(res => res.data);
+  return res;
+})
+
+export const ingredientsSlice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getIngredients.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getIngredients.fulfilled, (state, action: PayloadAction<Array<TIngredient>>) => {
+        state.isLoading = false;
+        state.ingredients = action.payload;
+      })
+      .addCase(getIngredients.rejected, (state, action: UnknownAsyncThunkRejectedAction) => {
+        state.isLoading = false;
+        state.ingredients = initialState.ingredients;
+        state.error = action.error.message || initialState.error;
+      })
+  }
+})
+
+export default ingredientsSlice.reducer;
