0cfba69588ad68cea91f5f2a586bda56acdc06fb
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-08-10 02:39:01 +0300

    refactor: migrate order details component to typescript

---
+++ src/components/order-details/order-details.tsx
@@ -0,0 +1,22 @@
+import styles from "./order-details.module.scss";
+import orderCompleteImage from "../../images/done.png";
+
+interface IOrderDetails {
+  orderNumber: number | null;
+}
+
+function OrderDetails({ orderNumber }: IOrderDetails) {
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.identifier}>{orderNumber}</h3>
+      <p className={styles.subtitle}>идентификатор заказа</p>
+      <img className={styles.image} src={orderCompleteImage} alt="Галочка" />
+      <span className={styles.description}>Ваш заказ начали готовить</span>
+      <span className={`${styles.description} ${styles.grey}`}>
+        Дождитесь готовности на орбитальной станции
+      </span>
+    </div>
+  );
+}
+
+export default OrderDetails;
